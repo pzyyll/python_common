4e4dfe0fee069b8eee9aa8549151453c680e060d
ZHILICAI-mbp <pzyyll@gmail.com>
2024-05-14 22:24:51 +0800

    push

--- logging_helper.py
+++ logging_helper.py
@@ -3,9 +3,19 @@
 # @Description: logging helper
 
 import logging
+import logging.handlers
 import pathlib
 import sys
 import inspect
+
+
+def setup_logging_file(filename):
+    handler = logging.handlers.RotatingFileHandler(
+        filename, maxBytes=10*1024*1024, backupCount=5)
+    logging.basicConfig(
+        handlers=[handler],
+        level=logging.INFO
+    )
 
 
 def init_logging(log_file, log_level, **kwargs):
@@ -14,12 +24,10 @@ def init_logging(log_file, log_level, **kwargs):
         if getattr(sys, 'frozen', False):
             pathObj = pathlib.Path(sys.executable, pathObj)
         else:
-            pathObj = pathlib.Path(inspect.stack()[1].filename).parent / pathObj
+            pathObj = pathlib.Path(
+                inspect.stack()[1].filename).parent / pathObj
     logging.basicConfig(
-        filename=pathObj.resolve(), 
-        encoding='utf-8', 
-        level=log_level, 
+        filename=pathObj.resolve(),
+        encoding='utf-8',
+        level=log_level,
         **kwargs)
-
-
-
